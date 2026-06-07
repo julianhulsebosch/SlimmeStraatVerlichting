f0acc1e52041049393505b18cdbb4cee4afa25ef
Julian <65360363+julianhulsebosch@users.noreply.github.com>
2026-06-07 15:14:02 +0200

    Add files via upload

---
+++ build/default/production/mcc_generated_files/uart/src/usart3.o.d
@@ -0,0 +1,159 @@
+build/default/production/mcc_generated_files/uart/src/usart3.o.d \
+ build/default/production/mcc_generated_files/uart/src/usart3.o: \
+ mcc_generated_files/uart/src/usart3.c \
+ mcc_generated_files/uart/src/../usart3.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stdbool.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdint.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\bits\alltypes.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\bits\stdint.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdio.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\features.h \
+ mcc_generated_files/uart/src/../../system/system.h \
+ mcc_generated_files/uart/src/../../system/../system/utils/compiler.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\inttypes.h \
+ C:/Program\ Files/Microchip/MPLABX/v6.25/packs/Microchip/AVR-Dx_DFP/2.7.321/xc8/avr/include/avr/ioavr128db48.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\xmega.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\builtins.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdlib.h \
+ mcc_generated_files/uart/src/../../system/../system/utils/interrupt_avr8.h \
+ c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\interrupt.h \
+ mcc_generated_files/uart/src/../../system/config_bits.h \
+ mcc_generated_files/uart/src/../../system/../system/clock.h \
+ mcc_generated_files/uart/src/../../system/../system/ccp.h \
+ mcc_generated_files/uart/src/../../system/../system/../system/utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../system/../system/protected_io.h \
+ mcc_generated_files/uart/src/../../system/../system/pins.h \
+ mcc_generated_files/uart/src/../../system/../system/./port.h \
+ mcc_generated_files/uart/src/../../system/../system/./utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../evsys/evsys.h \
+ mcc_generated_files/uart/src/../../system/../evsys/../system/utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../timer/rtc.h \
+ mcc_generated_files/uart/src/../../system/../timer/../system/utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../uart/usart3.h \
+ mcc_generated_files/uart/src/../../system/../vref/vref.h \
+ mcc_generated_files/uart/src/../../system/../vref/../system/utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../system/interrupt.h \
+ mcc_generated_files/uart/src/../../system/../system/../system/utils/atomic.h \
+ mcc_generated_files/uart/src/../../system/../adc/adc0.h \
+ mcc_generated_files/uart/src/../../system/../adc/../system/utils/compiler.h \
+ mcc_generated_files/uart/src/../../system/../adc/./adc_types.h \
+ mcc_generated_files/uart/src/../../system/../timer/tca0.h \
+ mcc_generated_files/uart/src/../../system/../timer/./tca0_deprecated.h \
+ mcc_generated_files/uart/src/../../system/../timer/tcb0.h \
+ mcc_generated_files/uart/src/../../system/../timer/tcb0_deprecated.h \
+ mcc_generated_files/uart/src/../uart_drv_interface.h \
+ mcc_generated_files/uart/src/../uart_types.h
+
+mcc_generated_files/uart/src/../usart3.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\bits\alltypes.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\bits\stdint.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdio.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\features.h:
+
+mcc_generated_files/uart/src/../../system/system.h:
+
+mcc_generated_files/uart/src/../../system/../system/utils/compiler.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\inttypes.h:
+
+C:/Program\ Files/Microchip/MPLABX/v6.25/packs/Microchip/AVR-Dx_DFP/2.7.321/xc8/avr/include/avr/ioavr128db48.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\builtins.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\stdlib.h:
+
+mcc_generated_files/uart/src/../../system/../system/utils/interrupt_avr8.h:
+
+c:\program\ files\microchip\xc8\v3.10\avr\avr\include\avr\interrupt.h:
+
+mcc_generated_files/uart/src/../../system/config_bits.h:
+
+mcc_generated_files/uart/src/../../system/../system/clock.h:
+
+mcc_generated_files/uart/src/../../system/../system/ccp.h:
+
+mcc_generated_files/uart/src/../../system/../system/../system/utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../system/../system/protected_io.h:
+
+mcc_generated_files/uart/src/../../system/../system/pins.h:
+
+mcc_generated_files/uart/src/../../system/../system/./port.h:
+
+mcc_generated_files/uart/src/../../system/../system/./utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../evsys/evsys.h:
+
+mcc_generated_files/uart/src/../../system/../evsys/../system/utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../timer/rtc.h:
+
+mcc_generated_files/uart/src/../../system/../timer/../system/utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../uart/usart3.h:
+
+mcc_generated_files/uart/src/../../system/../vref/vref.h:
+
+mcc_generated_files/uart/src/../../system/../vref/../system/utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../system/interrupt.h:
+
+mcc_generated_files/uart/src/../../system/../system/../system/utils/atomic.h:
+
+mcc_generated_files/uart/src/../../system/../adc/adc0.h:
+
+mcc_generated_files/uart/src/../../system/../adc/../system/utils/compiler.h:
+
+mcc_generated_files/uart/src/../../system/../adc/./adc_types.h:
+
+mcc_generated_files/uart/src/../../system/../timer/tca0.h:
+
+mcc_generated_files/uart/src/../../system/../timer/./tca0_deprecated.h:
+
+mcc_generated_files/uart/src/../../system/../timer/tcb0.h:
+
+mcc_generated_files/uart/src/../../system/../timer/tcb0_deprecated.h:
+
+mcc_generated_files/uart/src/../uart_drv_interface.h:
+
+mcc_generated_files/uart/src/../uart_types.h:
